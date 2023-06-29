5429d18de5c5b4fb1b8a37497732936a384db554
BntngSantosa <bintangsantosa11@gmail.com>
2023-06-29 12:52:00 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# SimpleTodoList
+This is a simple Todo List application built with HTML, CSS, and JavaScript. It allows users to store and manage their important tasks and notes.
+
+Features
+Add new tasks to the list by entering them in the provided textarea and clicking the "Save" button.
+Each task is displayed as a list item with an option to delete it.
+Deleting a task requires confirmation to ensure the user's intention.
+The application utilizes local storage to persist the task list, ensuring data is retained even after page reloads.
+The UI is responsive and user-friendly, providing an intuitive experience for managing tasks.
+
+Usage
+Clone the repository:
+Copy code
+git clone https://github.com/BntngSantosa/SimpleTodoList.git
+Open the index.html file in your web browser.
+
+Add tasks by typing them into the textarea and clicking the "Save" button.
+
+To delete a task, click the "X" button next to the task item and confirm the deletion.
+
+Contributing
+Contributions are welcome! If you have any ideas, improvements, or bug fixes, please feel free to open an issue or submit a pull request.
+
+License
+This project is licensed under the MIT License.
+
+Acknowledgments
+This project was inspired by the need for a simple and effective way to manage daily tasks.
+Special thanks to the contributors and developers who have provided valuable feedback and suggestions to improve this application.
+Feel free to customize the README.md file according to your specific repository setup and requirements.
---
+++ src/js/script.js
@@ -0,0 +1,128 @@
+const btn = document.querySelector(".btn");
+const list = document.querySelector(".text-list");
+
+btn.addEventListener("click", () => {
+  const tArea = document.querySelector("#t-area");
+  const text = tArea.value;
+  const failed = document.getElementById("failednone");
+  const btnFailed = document.querySelector(".btn-failed");
+  const success = document.getElementById("successnone");
+  const btnSuccess = document.querySelector(".btn-success");
+
+  if (text == "") {
+    failed.id = "failed";
+    return btnFailed.addEventListener("click", () => {
+      failed.id = "failednone";
+    });
+  }
+
+  success.id = "success";
+
+  btnSuccess.addEventListener("click", () => {
+    success.id = "successnone";
+  });
+
+  const newUl = document.createElement("ul");
+
+  const newLi = document.createElement("li");
+  const tLi = document.createTextNode(text);
+
+  const newSpan = document.createElement("span");
+  const tSpan = document.createTextNode("X");
+
+  newSpan.appendChild(tSpan);
+  newSpan.setAttribute("class", "delete");
+
+  newLi.appendChild(tLi);
+
+  newUl.appendChild(newLi);
+  newUl.appendChild(newSpan);
+
+  list.appendChild(newUl);
+
+  tArea.value = "";
+
+  // Menyimpan data ke localStorage
+  const savedData = localStorage.getItem("savedData") ? JSON.parse(localStorage.getItem("savedData")) : [];
+  savedData.push(text);
+  localStorage.setItem("savedData", JSON.stringify(savedData));
+});
+
+// Memulihkan data saat halaman dimuat
+document.addEventListener("DOMContentLoaded", () => {
+  const savedData = localStorage.getItem("savedData");
+
+  if (savedData) {
+    const data = JSON.parse(savedData);
+
+    data.forEach((text) => {
+      const newUl = document.createElement("ul");
+
+      const newLi = document.createElement("li");
+      const tLi = document.createTextNode(text);
+
+      const newSpan = document.createElement("span");
+      const tSpan = document.createTextNode("X");
+
+      newSpan.appendChild(tSpan);
+      newSpan.setAttribute("class", "delete");
+
+      newLi.appendChild(tLi);
+
+      newUl.appendChild(newLi);
+      newUl.appendChild(newSpan);
+
+      list.appendChild(newUl);
+    });
+  }
+});
+
+list.addEventListener("click", (e) => {
+
+  const container = document.querySelector("#containernone");
+  const btnOke = document.querySelector(".btn-oke");
+  const btnCancl = document.querySelector(".btn-cancel");
+
+  if (e.target.classList.contains("delete")) {
+
+    container.id = "container";
+
+    btnOke.addEventListener("click", () => {
+
+      e.target.parentElement.remove();
+      container.id = "containernone";
+
+      // Menghapus data dari localStorage
+      const savedData = localStorage.getItem("savedData");
+
+      if (savedData) {
+
+        const data = JSON.parse(savedData);
+        const text = e.target.previousSibling.textContent;
+        const index = data.indexOf(text);
+
+        if (index !== -1) {
+          data.splice(index, 1);
+          localStorage.setItem("savedData", JSON.stringify(data));
+        }
+      }
+    });
+  }
+
+  btnCancl.addEventListener("click", () => {
+    
+    container.id = "containernone";
+  });
+});
+
+// darkmode
+const body = document.body;
+const i = document.querySelector("nav i");
+
+i.addEventListener("click", () => {
+
+  i.classList.toggle("fa-sun");
+  i.classList.toggle("fa-moon");
+  body.classList.toggle("dark-mode");
+
+});
